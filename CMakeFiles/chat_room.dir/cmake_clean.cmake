40a7f56ef4b0b8c7c8bca52d0f4f04a738f08cec
ynwad <qingchenchn@gmail.com>
2024-04-23 22:22:41 +0800

    add chatroot

---
+++ CMakeFiles/chat_room.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chat_room.dir/chat/chat_servlet.cc.o"
+  "CMakeFiles/chat_room.dir/chat/my_module.cc.o"
+  "CMakeFiles/chat_room.dir/chat/protocol.cc.o"
+  "CMakeFiles/chat_room.dir/chat/resource_servlet.cc.o"
+  "lib/libchat_room.pdb"
+  "lib/libchat_room.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chat_room.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
